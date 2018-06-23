df8ff1c24f83cd696f891415dbfd4237acf92bec
gutfeeling <dibyachakravorty@gmail.com>
2018-06-23 14:13:20 +0200

    change DEBUG from heroku env var

--- ki_labs_backend/ki_labs_backend/settings/settings_heroku.py
+++ ki_labs_backend/ki_labs_backend/settings/settings_heroku.py
@@ -1,6 +1,7 @@
-from .settings_base import *
-
+from django.core.exceptions import ImproperlyConfigured
 import dj_database_url
+
+from .settings_base import *
 
 STATIC_ROOT = BASE_DIR.child("staticfiles")
 
@@ -8,5 +9,13 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 DEBUG = False
+try:
+    heroku_debug = get_environment_variable("DEBUG")
+    if heroku_debug == "True":
+        DEBUG = True
+except ImproperlyConfigured:
+    pass
+
+
 SECRET_KEY = get_environment_variable('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = ['ki-labs.herokuapp.com']
