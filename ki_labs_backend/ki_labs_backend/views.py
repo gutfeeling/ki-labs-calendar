ceee713e2f4e32b1c396712a1a763e6dc5275d2e
gutfeeling <dibyachakravorty@gmail.com>
2018-06-24 10:46:28 +0200

    document the welcome endpoint

--- ki_labs_backend/ki_labs_backend/views.py
+++ ki_labs_backend/ki_labs_backend/views.py
@@ -2,6 +2,18 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 class WelcomeView(APIView):
+    """
+    get:
+    Returns information about the API.
+
+    ###Response schema
+        {
+            "name" : Name of the API,
+            "version" : Version number of the API,
+            "description" : A few lines about what the API can do,
+            "documentation" : The relative URL where the API is documented
+            }
+    """
 
     def get(self, request, format = None):
 
@@ -15,7 +27,7 @@ class WelcomeView(APIView):
                 "candidate and a group of interviewers are all available "
                 "together."
                 ),
-            "documentation" : "/docs",
+            "documentation" : "/docs/",
             }
 
         return Response(response)
